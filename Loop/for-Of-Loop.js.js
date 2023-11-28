f567964703fc30106b475d513e0dc3e2654a5ad5
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2023-11-29 03:14:17 +0530

    Add files via upload

---
+++ Loop/for-Of-Loop.js.js
@@ -0,0 +1,9 @@
+
+let names = "Achyutam Sharma";
+let size = 0;
+for (let variables of names) {
+    console.log(variables);
+    size++;
+}
+console.log("String = ", size);
+// it also include space.
---
+++ Loop/for-loop.js.js
@@ -0,0 +1,21 @@
+function fabonacci() {
+
+var arrays = [];    
+var x = prompt("Write a number.");
+var a = 0;
+var b = 1;
+var c;
+
+for (let n = 0; n < x; n++){  
+
+   a; 
+   c = a + b;
+   a = b;
+   b = c;
+
+    
+    console.log(b);
+    
+}
+}
+console.log(fabonacci());
