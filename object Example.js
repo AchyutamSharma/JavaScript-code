2b7e09b62222098884870d56b67f4129b25152be
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2023-12-03 19:42:35 +0530

    Add files via upload

---
+++ object Example.js
@@ -0,0 +1,64 @@
+/*
+const student = {
+    fullName :"Rahul",
+    age : 22,
+    cgpa : 8.02,
+    isPass : true,
+};
+*/
+
+/*
+function ProductofAmazon() {
+const Pen = {
+    Name : "Parker Pen",
+    color : "Blue",
+    rating : 4,
+    MRP : 270,
+    isOffer : true,
+}
+const Speaker = {
+    Name : "Boat",
+    color : "Black",
+    rating : 4,
+    MRP : 999,
+    isOffer : true,
+}
+    const Laptop = {
+    Name : "HP Victus",
+    color : "Black",
+    rating : 4,
+    MRP : 69999,
+    isOffer : true,
+}
+
+    let wants = prompt("Which Product do you want from Amazon? \n we have this product : Pen, Laptop, Speaker. \n Type any of this product...");
+
+    switch(wants){
+
+        case 'laptop':
+            alert(Laptop);    
+        break;
+        case 'speaker':
+            alert(Speaker);    
+        break;   
+        case 'pen':
+            alert(Pen);    
+        break;  
+
+        default: alert(`Sorry, we are out of this ${wants}. `);   
+    }
+
+}
+console.log(ProductofAmazon());
+*/
+
+const profile = {
+    UserName : '@shradhakhapra',
+    Name : 'Shradha Khapra',
+    isFollow : true,
+    Post : 195,
+    Following :  4,
+    Followers : 569000 ,
+    
+};
+console.log(profile);
---
+++ operators.js.js
@@ -0,0 +1,59 @@
+let a = 5;
+let b = 3;
+
+/* Arrithematic */
+console.log("1. a = ", a, " & b = ", b);
+console.log("2. a + b = ", a + b); 
+console.log("3. a - b = ", a - b); 
+console.log("4. a / b = ", a / b); 
+console.log("5. a * b = ", a * b);
+console.log("6. a % b = ", a % b); // remainder
+console.log("7. a ** b = ", a ** b); // a^b  
+
+
+/* Unary Operator */
+/* Increment or Decrement */
+
+console.log("8. a++ = ", a++); /* a++ (post Increment) */
+console.log("9. ",a);
+console.log("In line number 9 it change value After Executing(8. line).");
+console.log("10. ++a = ", ++a); /* ++a (Pre Increment) */
+console.log("11. ", a);
+console.log("In line number 10 it change value Before Executing.");
+console.log("12. a-- = ", a--);  /* a-- (post Decrement) */
+console.log("13. ", a);
+console.log("In line number 13 it change value After Executing(12. line).");
+console.log("14. --a = ", --a);  /* --a (Pre Decrement) */
+console.log("15. ", a);
+console.log("In line number 14 it change value Before Executing.");
+
+/* Asignment Operator */
+/*
+a += 4;
+a *= 4;
+a -= 4;
+a /= 4;
+a %= 4;
+a **= 4; //625 [5*5*5*5]
+*/
+
+/* Comparison */
+/*
+Equal-to     "==" ;
+Not-Equal-to  "!=" ;
+Equal-to-Type  "===";   compare all even datatype also. 
+Not-Equal-to-Type  "!==";   same as above.
+
+>, >=, <=, <;
+
+*/
+
+/* Logical Operator */
+/*
+Logical AND  '&&' ;    :- if both are True then it print true otherwise False
+Logical OR   '||' ;    :- if any one is then true it print true
+Logical Not  '!' ;    :- It change to True  = False , False = True ;
+*/
+
+
+
